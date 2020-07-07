7d4a58ef9432460362111aa44122f373442d3c91
jinrongbin <jinrongbin@youbao360.com>
2020-07-07 14:50:02 +0800

    新增配号文件夹

---
+++ src/components/distribute/index/index.js
@@ -0,0 +1,18 @@
+import React, { component } from 'react';
+
+export default class diatribute extends React.component {
+    state = {
+        dataList: [],
+    };
+    componentWillMount() {
+        document.title = "币票配号";
+    };
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+
+}
---
+++ src/components/distribute/search/search.js
@@ -0,0 +1,18 @@
+import React, { component } from 'react';
+
+export default class search extends React.component {
+    state = {
+        dataList: [],
+    };
+    componentWillMount() {
+        document.title = "配号搜索";
+    };
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+
+}
