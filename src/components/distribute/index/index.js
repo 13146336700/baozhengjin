b2ce2b1a8c594e2a67ae4f466eb7724431994041
jinrongbin <jinrongbin@youbao360.com>
2020-07-11 19:03:45 +0800

    banner图和APP交互调试

--- src/components/distribute/index/index.js
+++ src/components/distribute/index/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./index.scss";
 import Demo from "../pullRefresh/pullRefresh";
 import Banner from "../banner/banner";
+import Uheader from "../../Goolbal/Uheader";
 
 
 export default class distribute extends React.Component {
@@ -14,6 +15,7 @@ export default class distribute extends React.Component {
     render() {
         return (
             <div className="distribute" style={{padding:'10px 0 0 0'}}>
+                <Uheader {...this.props} utitle="配号出售发布" useach="true"></Uheader>
                 <Banner rpType="ybBanner"/>
                 <Demo page="index" />
                 <div className="pub">
